913b30cc40d6ab0a88f3bc7617e58b4de7586830
JashanB <jashan_brar50@hotmail.com>
2020-08-26 19:53:34 -0400

    added react

---
+++ my-store/src/components/app/App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+
+function App() {
+  const divStyle = {
+    padding: '177.7778% 5px 5px 5px',
+    background: 'rgba(0,0,0,0.03)',
+    // border-radius: '8px'
+  }
+  // const backgroundStyle = {
+  //   backgroundImage = 
+  // }
+  return (
+    // <Router>
+    // <div>
+    //   <h1 className="yoga-logo">Yoga-Me</h1>
+    //   {/* <div className="App">
+    //     <Switch>
+    //       <Route path='/users/:user_id/places/:id' render={(props) => <Place {...props} />} />
+    //       <Route path='/users/:id' render={(props) => <Search {...props} />} />
+    //     </Switch>
+    //   </div> */}
+
+    // {/* </Router> */}
+    // </div>
+    <div className="App">
+    {/* <img className="logo-img" src="yogalogo.png"></img>
+    <img className="main-header-img" src="yogaheader.png"></img>
+    <img className="yoga-background-img"src="yoga.png"></img> */}
+    </div>
+  );
+}
+
+export default App;
