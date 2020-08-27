8809b532fc9f0661c422b0eaa0de9cd778d6a13b
JashanB <jashan_brar50@hotmail.com>
2020-08-26 21:50:13 -0400

    added data from fakestoreapi

--- my-store/src/components/app/App.js
+++ my-store/src/components/app/App.js
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 
 function App() {
+  const [state, setState] = useState({
+    store: []
+  });
   const divStyle = {
     padding: '177.7778% 5px 5px 5px',
     background: 'rgba(0,0,0,0.03)',
     // border-radius: '8px'
   }
+  useEffect(() => {
+    axios.get('https://fakestoreapi.com/products')
+    .then(res => {
+      console.log(res.data)
+      setState({
+        store: [...state.store, res.data]
+      });
+    })
+  }, [state])
   // const backgroundStyle = {
   //   backgroundImage = 
   // }
@@ -26,6 +39,9 @@ function App() {
     // {/* </Router> */}
     // </div>
     <div className="App">
+      <span>
+        {/* {state} */}
+      </span>
     {/* <img className="logo-img" src="yogalogo.png"></img>
     <img className="main-header-img" src="yogaheader.png"></img>
     <img className="yoga-background-img"src="yoga.png"></img> */}
--- server.js
+++ server.js
@@ -5,19 +5,31 @@ const app = express(),
 
 // place holder for the data
 const users = [];
-
+const fetch = require("node-fetch");
 app.use(bodyParser.json());
 
-app.get('/api/users', (req, res) => {
-  console.log('api/users called!!!!')
-  res.json(users);
+app.get('/api/store', (req, res) => {
+  // console.log('api/users called!!!!')
+  // res.json(users);
+   fetch('https://fakestoreapi.com/products')
+   .then(res=>res.json())
+   .then(json=>{
+     console.log(json)
+     for (i = 0; i < json.length; i++) {
+       console.log(json[i])
+     }
+    })
 });
 
-app.post('/api/user', (req, res) => {
-  const user = req.body.user;
-  console.log('Adding user::::::::', user);
-  users.push(user);
-  res.json("user addedd");
+app.post('/api/store', (req, res) => {
+  // const user = req.body.user;
+  // console.log('Adding user::::::::', user);
+  // users.push(user);
+  // res.json("user addedd");
+  // axios.get('https://fakestoreapi.com/products')
+    // .then(res => {
+    //   console.log(res.data)
+    // })
 });
 
 app.get('/', (req,res) => {
